109cd9680f82f20e72c13864d194cc9ff7343a82
bkhofmann <benkb1ahr-github@yahoo.com>
2024-12-18 09:26:36 -0500

    add back M82 in start gcode

--- RetCalMain.py
+++ RetCalMain.py
@@ -185,6 +185,7 @@ def gengcode ():
         file.write(f"PRINT_START EXTRUDER_TEMP={int(tsh)} BED_TEMP={int(tb)} MATERIAL=PLA\n")
         file.write(f"SET_PRESSURE_ADVANCE EXTRUDER=extruder ADVANCE=0.9\n")
         file.write(f"SET_GCODE_OFFSET Z=-0.06\n")
+        file.write(f"M82\n")
         
         file.write(f"{sgcode}\n")
 
